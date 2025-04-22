cf14c4c1b6ac8cb418895f71c4d38dce9044cc1e
Skssssee <91522489+Skssssee@users.noreply.github.com>
2025-04-22 18:41:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import environ 
 
 class Config:
-    API_ID = environ.get("API_ID", "577678")
-    API_HASH = environ.get("API_HASH", "d2c6e01uuiuiouioiuiou0fc6d7a1be")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "70955...") 
+    API_ID = environ.get("API_ID", "27479878")
+    API_HASH = environ.get("API_HASH", "05f8dc8265d4c5df6376dded1d71c0ff")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "8139926505:AAFUbR35uZx4AKeJ42m3Z52TFw3k5vboiNc") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://chhjgjkkjhkjhkjh@cluster0.xowzpr4.mongodb.net/")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6964148334').split()]
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://jowan20678:qGcqHCgAOiYnwG9K@cluster0sddji.3cetple.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0sddji")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0sddji")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7613349267').split()]
 
 class temp(object): 
     lock = {}
